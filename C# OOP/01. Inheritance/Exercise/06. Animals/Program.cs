72fdd0b6f7423de2e78c221e94ed71f5212000ab
Bozhikov99 <registracia103@abv.bg>
2021-02-26 12:52:49 +0200

    Adding exercises.

---
+++ C# OOP/01. Inheritance/Exercise/06. Animals/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace Animals
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Animal> animals = new List<Animal>();
+            string cmd = Console.ReadLine();
+            while (cmd!="Beast!")
+            {
+                string[] tokens = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string name = tokens[0];
+                int age = int.Parse(tokens[1]);
+                string gender = tokens[2];
+
+
+                if (string.IsNullOrEmpty(name) ||
+                    int.Parse(tokens[1]) < 0 ||
+                    string.IsNullOrEmpty(gender))
+
+                {
+                    Console.WriteLine("Invalid input!");
+                }
+                else
+                {
+                    if (cmd == "Dog")
+                    {
+                        Dog current = new Dog(name, age, gender);
+                        Console.WriteLine(current);
+                        Console.WriteLine(current.ProduceSound());
+                    }
+                    else if (cmd == "Cat")
+                    {
+                        Cat currentCat = new Cat(name, age, gender);
+                        Console.WriteLine(currentCat);
+                        Console.WriteLine(currentCat.ProduceSound());
+                    }
+                    else if (cmd == "Frog")
+                    {
+                        Frog currentFrog = new Frog(name, age, gender);
+                        Console.WriteLine(currentFrog);
+                        Console.WriteLine(currentFrog.ProduceSound());
+                    }
+                    else if (cmd == "Tomcat")
+                    {
+                        Tomcat currentTom = new Tomcat(name, age);
+                        Console.WriteLine(currentTom);
+                        Console.WriteLine(currentTom.ProduceSound());
+                    }
+                    else if (cmd=="Kitten")
+                    {
+                        Kitten currentKitty = new Kitten(name, age);
+                        Console.WriteLine(currentKitty);
+                        Console.WriteLine(currentKitty.ProduceSound());
+                    }
+                            
+                }
+                cmd = Console.ReadLine();
+            }
+            
+        }
+    }
+}
