41c95067e3f0e459b6ef51511a6b9352a67821b3
Bozhikov99 <registracia103@abv.bg>
2021-02-28 22:38:49 +0200

    Adding exercises.

---
+++ C# OOP/02. Encapsulation/Exercise/03. Shopping Spree/Person.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingSpree
+{
+    public class Person
+    {
+        public Person(string name, decimal money)
+        {
+            bag = new List<Product>();
+            Name = name;
+            Money = money;
+        }
+        private string name;
+        private decimal money;
+        List<Product> bag;
+
+        public string Name
+        {
+            get => name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+                name = value;
+            }
+        }
+
+        public decimal Money
+        {
+            get => money;
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+                money = value;
+            }
+        }
+
+        public void BuyProduct(Product product)
+        {
+            if (money-product.Cost < 0)
+            {
+                Console.WriteLine($"{Name} can't afford {product.Name}");
+            }
+            else
+            {
+                money -= product.Cost;
+                bag.Add(product);
+                Console.WriteLine($"{Name} bought {product.Name}");
+            }
+        }
+
+        public override string ToString()
+        {
+            if (bag.Count>0)
+            {
+            return $"{Name} - {string.Join(", ", bag)}";
+            }
+            return $"{Name} - Nothing bought";
+        }
+    }
+}
