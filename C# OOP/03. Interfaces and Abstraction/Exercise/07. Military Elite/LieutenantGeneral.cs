eaefff75f844340ebd1a7177ee5cf1c1058f05c0
Bozhikov99 <registracia103@abv.bg>
2021-03-05 21:33:23 +0200

    Adding exercise.

---
+++ C# OOP/03. Interfaces and Abstraction/Exercise/07. Military Elite/LieutenantGeneral.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+public class LeutenantGeneral : Private, ILieutenantGeneral
+{
+    public LeutenantGeneral(int id, string firstName, string lastName, decimal salary, List<ISoldier> privates)
+        : base(id, firstName, lastName, salary)
+    {
+        this.Privates = privates;
+    }
+
+    public List<ISoldier> Privates { get; }
+
+    public override string ToString()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.AppendLine(base.ToString())
+            .AppendLine($"Privates:");
+        foreach (ISoldier @private in this.Privates)
+        {
+            sb.AppendLine($"  {@private}");
+        }
+        return sb.ToString().Trim();
+    }
+}
