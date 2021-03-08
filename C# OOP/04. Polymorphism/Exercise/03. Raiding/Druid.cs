65ccb21e28e4fd52417d80cab610bfd4afe4f1c1
Bozhikov99 <registracia103@abv.bg>
2021-03-08 22:57:34 +0200

    Adding exercise.

---
+++ C# OOP/04. Polymorphism/Exercise/03. Raiding/Druid.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Raiding
+{
+    public class Druid : BaseHero
+    {
+        private const int defaultPower = 80;
+        public Druid(string name) :
+            base(name, defaultPower)
+        {
+        }
+
+        public override string CastAbility()
+        {
+            return $"{GetType().Name} - {Name} healed for {Power}";
+        }
+    }
+}
---
+++ C# OOP/04. Polymorphism/Exercise/03. Raiding/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Raiding
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<BaseHero> raiders = new List<BaseHero>();
+
+            while (raiders.Count < n)
+            {
+                string name = Console.ReadLine();
+                string type = Console.ReadLine();
+                try
+                {
+                    BaseHero hero = CharacterCreation(name, type);
+                    raiders.Add(hero);
+                }
+                catch (ArgumentException x)
+                {
+                    Console.WriteLine(x.Message);
+                }
+            }
+
+            int bossPower = int.Parse(Console.ReadLine());
+
+            foreach (BaseHero b in raiders)
+            {
+                Console.WriteLine(b.CastAbility());
+                bossPower -= b.Power;
+            }
+
+            string output = bossPower <= 0 ? "Victory!" : "Defeat...";
+            Console.WriteLine(output);
+
+        }
+            public static BaseHero CharacterCreation(string name, string type)
+            {
+                BaseHero current = null;
+
+                switch (type)
+                {
+                    case "Paladin":
+                        current = new Paladin(name);
+                        break;
+                    case "Warrior":
+                        current = new Warrior(name);
+                        break;
+                    case "Druid":
+                        current = new Druid(name);
+                        break;
+                    case "Rogue":
+                        current = new Rogue(name);
+                        break;
+                    default:
+                        throw new ArgumentException("Invalid hero!");
+                }
+                return current;
+            }
+    }
+}
