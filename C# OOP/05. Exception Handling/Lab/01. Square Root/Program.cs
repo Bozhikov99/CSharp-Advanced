d0bf53d471672f5de084e9c1163d89917d090e5e
Bozhikov99 <registracia103@abv.bg>
2021-03-09 21:20:44 +0200

    Adding exercise.

---
+++ C# OOP/05. Exception Handling/Lab/01. Square Root/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SquareRoot
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if (number<0)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                Console.WriteLine(Math.Sqrt(number));
+            }
+            catch (Exception x)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            finally
+            {
+                Console.WriteLine("Good bye");
+            }
+        }
+    }
+}
