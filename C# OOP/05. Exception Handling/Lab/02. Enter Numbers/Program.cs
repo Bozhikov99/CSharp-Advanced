58f1c9d64ac37e43a3afbd03fb4041990486dcb8
Bozhikov99 <registracia103@abv.bg>
2021-03-09 21:31:27 +0200

    Adding exercise.

---
+++ C# OOP/05. Exception Handling/Lab/02. Enter Numbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _02._Enter_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = 1;
+            int end = 15;
+
+            for (int i = 0; i < 10; i++)
+            {
+                try
+                {
+                    Console.WriteLine($"Enter {i+1}:");
+                    EnterNumber(start, end);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Invalid number");
+                    i = -1;
+                }
+            }
+        }
+
+        public static void EnterNumber(int start, int end)
+        {
+            int current = int.Parse(Console.ReadLine());
+
+            if (current<start||current>end)
+            {
+                throw new ArgumentException();
+            }
+        }
+    }
+}
