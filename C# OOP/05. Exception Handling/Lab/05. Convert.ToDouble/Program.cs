eb4d50811a093d78bce224c08ee52f747e776798
Bozhikov99 <registracia103@abv.bg>
2021-03-09 22:26:29 +0200

    Adding exercise.

---
+++ C# OOP/05. Exception Handling/Lab/05. Convert.ToDouble/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Numerics;
+
+namespace _05._Convert.ToDouble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			BigInteger big = BigInteger.Pow(10, 50);
+
+			try
+			{
+				double a = Convert.ToDouble(big);
+			}
+			catch (FormatException)
+			{
+				Console.WriteLine("The input isn't a number!");
+				throw;
+			}
+			catch (InvalidCastException)
+			{
+				Console.WriteLine("The input cannot be parsed to 'double'!");
+				throw;
+			}
+			catch (OverflowException)
+			{
+				Console.WriteLine($"The input number should be between {double.MinValue} and {double.MaxValue}!");
+				throw;
+			}
+			
+        }
+    }
+}
