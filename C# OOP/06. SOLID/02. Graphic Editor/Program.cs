346f0f4386c832cb9b93a37f914db45cfff7fe4d
Bozhikov99 <registracia103@abv.bg>
2021-03-13 21:44:51 +0200

    Adding refactored code.

---
+++ C# OOP/06. SOLID/02. Graphic Editor/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P02.Graphic_Editor
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<IShape> shapes = new List<IShape>
+            {
+                new Rectangle(),
+                new Circle(),
+                new Square()
+            };
+
+            foreach (IShape shape in shapes)
+            {
+                shape.Draw();
+            }
+        }
+    }
+}
