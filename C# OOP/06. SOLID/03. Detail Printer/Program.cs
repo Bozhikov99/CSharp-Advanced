4d036249a0036184d3e4c799337086ed9e26cf66
Bozhikov99 <registracia103@abv.bg>
2021-03-13 23:40:08 +0200

    Refactoring code.

---
+++ C# OOP/06. SOLID/03. Detail Printer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P03.DetailPrinter
+{
+    class Program
+    {
+        static void Main()
+        {
+            Manager manager = new Manager("Petar", new List<string> { "side note" });
+            Manager manager1 = new Manager("Kaloyan", new List<string> { "side note","VAC Ban reports" });
+
+            DetailsPrinter printer = new DetailsPrinter(new List<Employee> { manager, manager1 });
+            printer.PrintDetails();
+        }
+    }
+}
