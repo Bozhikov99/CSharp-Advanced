d3c30889cd57a902cec8ac5e235984d9032158bd
Bozhikov99 <registracia103@abv.bg>
2021-03-15 10:28:29 +0200

    Refactored the code from the exercise.

---
+++ C# OOP/06. SOLID/04. Recharge/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace P04.Recharge
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            Employee employee = new Employee("18061017");
+            employee.Work(8);
+
+            Robot stancho = new Robot("0013", 30);
+            stancho.Recharge();
+            stancho.Work(8);
+            stancho.Work(8);
+            stancho.Work(8);
+            stancho.Work(8);
+            
+        }
+    }
+}
