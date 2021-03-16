8d27d3f29a07c14cd2e96cf69bba9f6667d1eeec
Bozhikov99 <registracia103@abv.bg>
2021-03-16 23:18:23 +0200

    Adding exercise.

---
+++ C# OOP/07. Reflection and Attributes/Lab/01. Stealer/Spy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string StealFieldInfo(string className, params string[] fieldNames)
+        {
+            Type classType=Type.GetType(className);
+
+            FieldInfo[] fields = classType.GetFields(BindingFlags.NonPublic|
+                BindingFlags.Static|
+                BindingFlags.Instance|
+                BindingFlags.Public);
+
+            StringBuilder sb = new StringBuilder();
+
+            object instance = Activator.CreateInstance(classType, new object[] { });
+
+            sb.AppendLine($"Class under investigation: {className}");
+            foreach (var f in fields)
+            {
+                sb.AppendLine($"{f.Name} = {f.GetValue(instance)}");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
