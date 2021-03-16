2e1e8f5e1d9ed2306ea344fee5fe9ed18159935b
Bozhikov99 <registracia103@abv.bg>
2021-03-16 23:38:58 +0200

    Adding exercise.

---
+++ C# OOP/07. Reflection and Attributes/Lab/02. High Quality Mistakes/Spy.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string StealFieldInfo(string className, params string[] fieldNames)
+        {
+            Type classType=Type.GetType(className);
+
+            FieldInfo[] fields = classType.GetFields(BindingFlags.NonPublic|
+                BindingFlags.Static|
+                BindingFlags.Instance|
+                BindingFlags.Public);
+
+            StringBuilder sb = new StringBuilder();
+
+            object instance = Activator.CreateInstance(classType, new object[] { });
+
+            sb.AppendLine($"Class under investigation: {className}");
+            foreach (var f in fields)
+            {
+                sb.AppendLine($"{f.Name} = {f.GetValue(instance)}");
+            }
+
+            return sb.ToString();
+        }
+
+        public string AnalyzeAccessModifiers(string className)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            Type typeName = Type.GetType(className);
+
+            FieldInfo[] fields = typeName.GetFields(
+                BindingFlags.Public|
+                BindingFlags.Instance|
+                BindingFlags.Static);
+
+            MethodInfo[] getters = typeName.GetMethods(BindingFlags.NonPublic|BindingFlags.Instance);
+            MethodInfo[] setters = typeName.GetMethods(BindingFlags.Public|BindingFlags.Instance);
+
+            foreach (FieldInfo field in fields)
+            {
+                sb.AppendLine($"{field.Name} must be private!");
+            }
+
+            foreach (MethodInfo m in getters.Where(m => m.Name.StartsWith("get")))
+            {
+                sb.AppendLine($"{m.Name} have to be public!");
+            }
+
+            foreach (MethodInfo m in setters.Where(m=>m.Name.StartsWith("set")))
+            {
+                sb.AppendLine($"{m.Name} have to be private!");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
