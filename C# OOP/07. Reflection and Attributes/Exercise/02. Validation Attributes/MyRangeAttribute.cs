294a493cdce8c25f72c1c3557149654aa9535917
Bozhikov99 <registracia103@abv.bg>
2021-03-19 15:25:27 +0200

    Adding exercise.

---
+++ C# OOP/07. Reflection and Attributes/Exercise/02. Validation Attributes/MyRangeAttribute.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ValidationAttributes
+{
+    public class MyRangeAttribute : MyValidationAttribute
+    {
+        private readonly int minValue;
+        private readonly int maxValue;
+
+        public MyRangeAttribute(int min, int max)
+        {
+            minValue = min;
+            maxValue = max;
+        }
+
+        public override bool IsValid(object obj)
+        {
+            int objectInt = (int)obj;
+
+            return objectInt >= minValue && objectInt <= maxValue;
+        }
+    }
+}
---
+++ C# OOP/07. Reflection and Attributes/Exercise/02. Validation Attributes/Validator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace ValidationAttributes
+{
+    public static class Validator
+    {
+        public static bool IsValid(object obj)
+        {
+            Type objectType = obj.GetType();
+
+            PropertyInfo[] properties = objectType.GetProperties();
+
+            foreach (PropertyInfo property in properties)
+            {
+                foreach (MyValidationAttribute attribute in property.GetCustomAttributes())
+                {
+                    if (!attribute.IsValid(property.GetValue(obj)))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
