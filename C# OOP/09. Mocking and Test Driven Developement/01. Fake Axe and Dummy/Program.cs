dd1a9fd0b38932b0d80781623a8373950bda3c08
Bozhikov99 <registracia103@abv.bg>
2021-03-24 23:23:17 +0200

    Adding exercise.

---
+++ C# OOP/09. Mocking and Test Driven Developement/01. Fake Axe and Dummy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01._Fake_Axe_and_Dummy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
