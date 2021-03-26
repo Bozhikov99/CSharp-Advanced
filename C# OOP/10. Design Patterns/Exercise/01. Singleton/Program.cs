ded954cd6a20d84d811f5cffbd8198acfb6d24f9
Bozhikov99 <registracia103@abv.bg>
2021-03-26 22:55:01 +0200

    Adding exercise.

---
+++ C# OOP/10. Design Patterns/Exercise/01. Singleton/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _01._Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = SingletonDataContainer.Instance;
+
+            Console.WriteLine(db.GetPopulation("Washington, D.C."));
+            Console.WriteLine(db.GetPopulation("London"));
+        }
+    }
+}
---
+++ C# OOP/10. Design Patterns/Exercise/01. Singleton/SingletonDataContainer.cs
@@ -0,0 +1,38 @@
+﻿using _01._Singleton.Contracts;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace _01._Singleton
+{
+    public class SingletonDataContainer : ISingletonContainer
+    {
+        private Dictionary<string, int> capitals= new Dictionary<string, int>();
+
+        private SingletonDataContainer()
+        {
+            Console.WriteLine("Initializing singleton object");
+
+            var elements = File.ReadAllLines("../../../capitals.txt");
+
+            for (int i = 0; i < elements.Length; i+=2)
+            {
+                string name = elements[i];
+                int population = int.Parse(elements[i + 1]);
+
+                capitals.Add(name,population);
+            }
+
+        }
+        public int GetPopulation(string name)
+        {
+            return capitals[name];
+        }
+
+        private static SingletonDataContainer instance = new SingletonDataContainer();
+
+        public static SingletonDataContainer Instance => instance;
+    }
+}
