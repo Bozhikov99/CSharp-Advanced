7164ff80bab3e3f2fa7396c5c9da6fc8911172a1
Bozhikov99 <registracia103@abv.bg>
2021-03-29 23:21:14 +0300

    Adding exercise.

---
+++ C# OOP/10. Design Patterns/Exercise/02. Composite/CompositeGift.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _02._Composite
+{
+    public class CompositeGift : GiftBase, IGiftOperations
+    {
+        private List<GiftBase> gifts;
+        public CompositeGift(string name, int price) 
+            : base(name, price)
+        {
+            gifts = new List<GiftBase>();
+        }
+
+        public void Add(GiftBase gift)
+        {
+            gifts.Add(gift);
+        }
+
+        public void Remove(GiftBase gift)
+        {
+            gifts.Remove(gift);
+        }
+
+        public override int CalculateTotalPrice()
+        {
+            int totalPrice = 0;
+
+            Console.WriteLine($"{name} contains the following products with prices:");
+
+            foreach (var item in gifts)
+            {
+                totalPrice += item.CalculateTotalPrice();
+            }
+
+            return totalPrice;
+        }
+
+    }
+}
