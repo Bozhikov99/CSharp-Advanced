e4e8762dfa10b6480c3c3e82665871bbbf3509d0
Bozhikov99 <registracia103@abv.bg>
2021-03-29 23:35:55 +0300

    Adding exercise.

---
+++ C# OOP/10. Design Patterns/Exercise/03. Template pattern/TwelveGrain.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03._Template_pattern
+{
+    public class TwelveGrain : Bread
+    {
+        public override void Bake()
+        {
+            Console.WriteLine("Baking the 12-Grain Bread. (25 minutes)");
+        }
+
+        public override void MixIngredients()
+        {
+            Console.WriteLine("Gathering Ingredients for 12-TwelveGrain Bread." );
+        }
+    }
+}
