2f2b6f01fd4b6dcd12541b4acf497178760163e3
Bozhikov99 <registracia103@abv.bg>
2021-03-29 23:04:22 +0300

    Adding exercise.

---
+++ C# OOP/10. Design Patterns/Exercise/01. Prototype/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _01._Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SandwichMenu menu = new SandwichMenu();
+
+            menu["BLT"] = new Sandwich("Wheat", "Bacon", "Lettuce", "Tomato");
+            menu["PB&J"] = new Sandwich("White", "Beef", "Iceberg", "Tomato");
+
+            Sandwich sandwich1 = menu["BLT"].Clone() as Sandwich;
+            Sandwich sandwich2 = menu["PB&J"].Clone() as Sandwich;
+        }
+    }
+}
---
+++ C# OOP/10. Design Patterns/Exercise/01. Prototype/SandwichMenu.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01._Prototype
+{
+    public class SandwichMenu
+    {
+        private readonly Dictionary<string, SandwichPrototype> sandwiches = new Dictionary<string, SandwichPrototype>();
+
+        public SandwichPrototype this[string name]
+        {
+            get => sandwiches[name];
+            set => sandwiches.Add(name,value);
+        }
+    }
+}
