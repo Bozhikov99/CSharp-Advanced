f65f84b7ad29f07d3380fa2a5b9d1c5266746cc5
Bozhikov99 <registracia103@abv.bg>
2021-04-02 23:32:14 +0300

    Preparing for exam...

---
+++ C# OOP/Exam Preparation/Exam - 12.12.2020/Bakery/Models/Tables/Table.cs
@@ -0,0 +1,123 @@
+﻿using Bakery.Models.BakedFoods.Contracts;
+using Bakery.Models.Drinks;
+using Bakery.Models.Drinks.Contracts;
+using Bakery.Models.Tables.Contracts;
+using Bakery.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+
+namespace Bakery.Models.Tables
+{
+    public abstract class Table : ITable
+    {
+        private int capacity;
+        private int numberOfPeople;
+
+        private readonly List<IBakedFood> foodOrders;
+        private readonly List<IDrink> drinkOrders;
+        protected Table(int tableNumber, int capacity, decimal pricePerPerson)
+        {
+            foodOrders = new List<IBakedFood>();
+            drinkOrders = new List<IDrink>();
+
+            TableNumber = tableNumber;
+            Capacity = capacity;
+            PricePerPerson = pricePerPerson;
+            IsReserved = false;
+        }
+        public int TableNumber { get; private set; }
+
+        public int Capacity
+        {
+            get => capacity;
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidTableCapacity);
+                }
+
+                capacity = value;
+            }
+        }
+
+        public int NumberOfPeople
+        {
+            get => numberOfPeople;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidNumberOfPeople);
+                }
+
+                numberOfPeople = value;
+            }
+        }
+
+        public decimal PricePerPerson { get; private set; }
+
+        public bool IsReserved { get; private set; }
+
+        public decimal Price { get => PricePerPerson * numberOfPeople; }
+
+        public void Clear()
+        {
+            IsReserved = false;
+            foodOrders.Clear();
+            drinkOrders.Clear();
+            numberOfPeople = 0;
+        }
+
+        public decimal GetBill()
+        {
+            decimal bill = 0;
+
+            foreach (var f in foodOrders)
+            {
+                bill += f.Price;
+            }
+
+            foreach (var d in drinkOrders)
+            {
+                bill += d.Price;
+            }
+
+            bill += Price;
+
+            return bill;
+        }
+
+        public string GetFreeTableInfo()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Table: {TableNumber}");
+            sb.AppendLine($"Type: {GetType().Name}");
+            sb.AppendLine($"Capacity: {Capacity}");
+            sb.AppendLine($"Price per Person: {PricePerPerson}");
+
+            return sb.ToString().Trim();
+        }
+
+        public void OrderDrink(IDrink drink)
+        {
+            drinkOrders.Add(drink);
+        }
+
+        public void OrderFood(IBakedFood food)
+        {
+            foodOrders.Add(food);
+        }
+
+        public void Reserve(int numberOfPeople)
+        {
+            IsReserved = true;
+
+            NumberOfPeople = numberOfPeople;
+        }
+    }
+}
